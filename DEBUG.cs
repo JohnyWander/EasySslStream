59f67dab30a135ce4fe23cc7622f819006e36e59
JohnyWander <j.myslinski.pl@gmail.com>
2023-07-11 14:52:18 +0200

    Preparation to refactorization

--- DEBUG.cs
+++ DEBUG.cs
@@ -12,32 +12,5 @@ using System.Threading.Tasks;
 
 namespace EasySslStream
 {
-    
-      public static class Program
-
-      {
-
-        static void Main()
-        {
-
-
-
-            TESTING.ServerSide serverTest = new TESTING.ServerSide();
-           
-            TESTING.Config.ServerTestConfig testconfig = new TESTING.Config.ServerTestConfig();
-            testconfig.ListenPort = 5000;
-            testconfig.ListenOnIpString = "127.0.0.1";
-            serverTest.LaunchTestServer(testconfig);
-            serverTest.StartTest(new TESTING.Config.ServerTestObjects() , ConnectionCommons.Unit.MBs, 1);
-
-
-
-            //Thread.Sleep(1000000);
-            
-        }
-
-
-
-        }
-
-    }
+ 
+ }
