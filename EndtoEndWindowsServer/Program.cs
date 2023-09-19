3f587ab06e95942331684cc0d597cd7fcf841e14
JohnyWander <j.myslinski.pl@gmail.com>
2023-09-19 21:10:39 +0200

    preparing to e2e and linux compatibility tests

---
+++ EndtoEndWindowsServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using EasySslStream.ConnectionV2.Server;
+
+namespace EndtoEndWindowsServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
