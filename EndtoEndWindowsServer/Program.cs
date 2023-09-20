aa25857d69beac1b2e598da74e45d3a2ab8c2680
JohnyWander <j.myslinski.pl@gmail.com>
2023-09-20 14:49:00 +0200

    starting work on end to end test server

--- EndtoEndWindowsServer/Program.cs
+++ EndtoEndWindowsServer/Program.cs
@@ -1,13 +1,29 @@
 ﻿using EasySslStream.ConnectionV2.Server;
+using System.Collections.Generic;
 
 namespace EndtoEndWindowsServer
 {
     internal class Program
     {
+        private struct SrvParam
+        {
+            public string ParamName;
+            public string ParamShortName;
+            public string ParamHelpMessage;
+
+            public string ParamValue;
+            public Action ParamAction;
+        }
+
+        private List<SrvParam> AvaiableParams;
+        private List<SrvParam> ActivatedParams;
+
+
         static void Main(string[] args)
         {
             
-        }
+            
 
+        }
     }
 }
